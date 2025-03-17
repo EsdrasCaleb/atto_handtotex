f9be0b7aaa348e7db3312a7d2f12d375bb656308
Esdras Caleb Oliveira Silva <esdrascaleb@gmail.com>
2025-03-17 19:10:35 -0300

    wip

---
+++ lang/en/atto_handtotex.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for the Atto HandToTex plugin.
+ *
+ * @package   atto_handtotex
+ * @copyright 2025 Caleb
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Hand to TeX';
+$string['handtotex'] = 'Draw Equation';
+$string['handtotex_desc'] = 'Convert hand-drawn equations to LaTeX';
+$string['handtotex_settings'] = 'Hand to TeX Settings';
+$string['handtotex_server_url'] = 'External Server URL';
+$string['handtotex_use_external'] = 'Use External Server for Better Recognition';
+$string['handtotex_draw_button'] = 'Draw Equation';
+$string['privacy:metadata'] = 'The Hand to TeX plugin does not store any personal data.';
